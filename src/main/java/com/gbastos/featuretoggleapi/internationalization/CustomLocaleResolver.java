ccbbe1d5c0fb7d4cde78a9c92c383a64faa2ced0
Guilherme Borges Bastos <guilhermeborgesbastos@gmail.com>
2020-06-17 18:51:34 -0300

    Created the Internationalization message files.
    - Created the messages.properties and messages_pt.properties files wrapping messages used on the API;
    - Created the Translator class used to read messages.properties files according to the 'Accept-Language' in the request's header.

---
+++ src/main/java/com/gbastos/featuretoggleapi/internationalization/CustomLocaleResolver.java
@@ -0,0 +1,32 @@
+package com.gbastos.featuretoggleapi.internationalization;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.i18n.AcceptHeaderLocaleResolver;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Locale;
+
+public class CustomLocaleResolver extends AcceptHeaderLocaleResolver implements WebMvcConfigurer {
+  List<Locale> LOCALES = Arrays.asList(new Locale("en"), new Locale("pt"));
+
+  @Override
+  public Locale resolveLocale(HttpServletRequest request) {
+    String headerLang = request.getHeader("Accept-Language");
+    return headerLang == null || headerLang.isEmpty()
+        ? Locale.getDefault()
+        : Locale.lookup(Locale.LanguageRange.parse(headerLang), LOCALES);
+  }
+
+  @Bean
+  public ResourceBundleMessageSource messageSource() {
+    ResourceBundleMessageSource resource = new ResourceBundleMessageSource();
+    resource.setBasename("messages");
+    resource.setDefaultEncoding("UTF-8");
+    resource.setUseCodeAsDefaultMessage(true);
+    return resource;
+  }
+}
---
+++ src/main/java/com/gbastos/featuretoggleapi/internationalization/Translator.java
@@ -0,0 +1,25 @@
+package com.gbastos.featuretoggleapi.internationalization;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.i18n.LocaleContextHolder;
+import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.stereotype.Component;
+
+import java.util.Locale;
+
+@Component
+public class Translator {
+
+  private static ResourceBundleMessageSource messageSource;
+
+  @Autowired
+  Translator(ResourceBundleMessageSource messageSource) {
+    Translator.messageSource = messageSource;
+  }
+
+  public static String toLocale(String msgCode) {
+    Locale locale = LocaleContextHolder.getLocale();
+    messageSource.setDefaultEncoding("ISO-8859-1");
+    return messageSource.getMessage(msgCode, null, locale);
+  }
+}
