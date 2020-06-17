2b3e77685784db78779ef2a3a15bf18c80a7e873
Guilherme Borges Bastos <guilhermeborgesbastos@gmail.com>
2020-06-17 18:56:28 -0300

    Created the custom enumerator validator.
    - Created the custom validator to used to validate the enumerator values on model object files (entities).

---
+++ src/main/java/com/gbastos/featuretoggleapi/validation/ValueOfEnum.java
@@ -0,0 +1,26 @@
+package com.gbastos.featuretoggleapi.validation;
+
+import javax.validation.Constraint;
+import javax.validation.Payload;
+import java.lang.annotation.Documented;
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+import static java.lang.annotation.ElementType.*;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
+@Target({METHOD, FIELD, ANNOTATION_TYPE, CONSTRUCTOR, PARAMETER, TYPE_USE})
+@Retention(RUNTIME)
+@Documented
+@Constraint(validatedBy = ValueOfEnumValidator.class)
+public @interface ValueOfEnum {
+  /** @return class containing enum values to which this String should match */
+  Class<? extends Enum<?>> enumClass();
+
+  /** @return the error message template */
+  String message() default "must be any of enum {enumClass}";
+  /** @return the groups the constraint belongs to */
+  Class<?>[] groups() default {};
+  /** @return the payload associated to the constraint */
+  Class<? extends Payload>[] payload() default {};
+}
