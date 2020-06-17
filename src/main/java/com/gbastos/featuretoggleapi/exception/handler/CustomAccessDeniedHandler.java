b212ba96323b1655af9085e5126701fbea162efb
Guilherme Borges Bastos <guilhermeborgesbastos@gmail.com>
2020-06-17 18:45:08 -0300

    Created the Exception Handler classes.
    - Created the classes for the Exception Handler which is used to stipulates how the APL will behavior when anomalous events disrupt the normal flow of that API's instructions;

---
+++ src/main/java/com/gbastos/featuretoggleapi/exception/handler/CustomAccessDeniedHandler.java
@@ -0,0 +1,18 @@
+package com.gbastos.featuretoggleapi.exception.handler;
+
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.web.access.AccessDeniedHandler;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class CustomAccessDeniedHandler implements AccessDeniedHandler {
+
+  @Override
+  public void handle(
+      HttpServletRequest httpServletRequest,
+      HttpServletResponse httpServletResponse,
+      AccessDeniedException e) {
+    throw e;
+  }
+}
