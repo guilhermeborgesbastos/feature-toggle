696908dadce2d9a89b9f0c0e6552f78ba69a7a70
Guilherme Borges Bastos <guilhermeborgesbastos@gmail.com>
2020-06-23 18:53:39 -0300

    Updated the main app module and routing.
    - Included the HTTP interceptor to the @NgModule of the 'app.module' file.
    - Updated the app routing to secured some routes based on the AuthGuard service.

--- src/main/resources/frontend/src/app/app-routing.module.ts
+++ src/main/resources/frontend/src/app/app-routing.module.ts
@@ -1,12 +1,20 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AuthGuard } from '@helpers/auth.guard';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
 
+// The home route is secured by passing the AuthGuard to the canActivate property of the route.
 const routes: Routes = [
-  { path: '', pathMatch: 'full', redirectTo: '/login'}
+  { path: '', component: HomeComponent, canActivate: [AuthGuard] },
+  { path: 'login', component: LoginComponent },
+
+  // otherwise redirect to home
+  { path: '**', redirectTo: '' },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/main/resources/frontend/src/app/app.module.ts
+++ src/main/resources/frontend/src/app/app.module.ts
@@ -1,19 +1,23 @@
-import {NgModule} from '@angular/core';
-import {CommonModule} from '@angular/common';
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 
-import {AppRoutingModule} from './app-routing.module';
-import {AppComponent} from './app.component';
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import {LoginModule} from './login/login.module';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+
+import { ErrorInterceptor } from '@helpers/error.interceptor';
+import { BasicAuthInterceptor } from '@helpers/basic-auth.interceptor';
+import { LoginModule } from './login/login.module';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
-  imports: [
-    AppRoutingModule,
-    BrowserAnimationsModule,
-    LoginModule,
-    CommonModule,
+  imports: [AppRoutingModule, BrowserAnimationsModule, LoginModule, CommonModule, HttpClientModule],
+  declarations: [AppComponent, HomeComponent],
+  providers: [
+    { provide: HTTP_INTERCEPTORS, useClass: BasicAuthInterceptor, multi: true },
+    { provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi: true },
   ],
-  declarations: [AppComponent],
   bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
