a03b5a1c9d11c0bda3da5545e580a149ea7369b7
Guilherme Borges Bastos <guilhermeborgesbastos@gmail.com>
2020-06-24 20:08:01 -0300

    Updated the AuthGuard to accept Roles on the Angular app.
    - Updated the AuthGuard to work with Roles (SUPER_ADMIN and PRODUCT_OWNER) according to each Route path.

--- src/main/resources/frontend/src/app/_helpers/auth.guard.ts
+++ src/main/resources/frontend/src/app/_helpers/auth.guard.ts
@@ -1,7 +1,16 @@
 import { Injectable } from '@angular/core';
-import { Router, CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
+import {
+  Router,
+  CanActivate,
+  ActivatedRouteSnapshot,
+  RouterStateSnapshot,
+  UrlTree,
+} from '@angular/router';
+import { map } from 'rxjs/operators';
+import { Role, User } from '@models/user';
 
 import { AuthenticationService } from '@services/authentication.service';
+import { Observable } from 'rxjs';
 
 /**
  * The auth guard is an angular route guard that's used to prevent unauthenticated users from accessing restricted routes,
@@ -11,17 +20,61 @@ import { AuthenticationService } from '@services/authentication.service';
  */
 @Injectable({ providedIn: 'root' })
 export class AuthGuard implements CanActivate {
-  constructor(private router: Router, private authenticationService: AuthenticationService) {}
+  constructor(private authentication: AuthenticationService, private router: Router) {
+    console.log('init guard');
+  }
 
-  canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
-    const user = this.authenticationService.userValue;
-    if (user) {
-      // logged in so return true
-      return true;
+  canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot
+  ): Observable<boolean> | Promise<boolean> | boolean {
+    console.log(`canActivate '${route.url}'`);
+    return this.canActivateRoute(route, state);
+  }
+
+  canActivateChild(
+    childRoute: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot
+  ): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+    console.log(`canActivate child '${childRoute.url}'`);
+    return this.canActivateRoute(childRoute, state);
+  }
+
+  private canActivateRoute(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot
+  ): Observable<boolean> {
+    return this.authentication.loggedUser$.pipe(
+      map((loggedUser) => {
+        const res = this.checkRoute(route, state, loggedUser);
+        console.log(`can activate route '${state.url}' '${route.url}' ${res}`);
+        return res;
+      })
+    );
+  }
+
+  private checkRoute(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot,
+    user: User
+  ): boolean {
+    if (
+      (!route.data.roles && user) ||
+      (route.data.roles && !user) ||
+      (user && route.data.roles && !route.data.roles.includes(user.role)) ||
+      state.url === '/'
+    ) {
+      if (user) {
+        if (user.role === Role.SUPER_ADMIN) {
+          this.router.navigate(['/admin/users']);
+        } else {
+          this.router.navigate(['/404']);
+        }
+      } else {
+        this.router.navigate(['/login']);
+      }
+      return false;
     }
-
-    // not logged in so redirect to login page with the return url
-    this.router.navigate(['/login'], { queryParams: { returnUrl: state.url } });
-    return false;
+    return true;
   }
 }
