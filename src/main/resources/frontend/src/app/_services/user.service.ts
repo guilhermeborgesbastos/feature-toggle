08bb8a5b0ad46c74853b5dc44354911e7d6f1382
Guilherme Borges Bastos <guilhermeborgesbastos@gmail.com>
2020-06-24 19:55:40 -0300

    Added findByEmail method the User Service on the Angular app.
    - Adjusted some routes according to the REST API.

--- src/main/resources/frontend/src/app/_services/user.service.ts
+++ src/main/resources/frontend/src/app/_services/user.service.ts
@@ -1,14 +1,24 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 
 import { environment } from '@environments/environment';
 import { User } from '@models/user';
+import { Observable } from 'rxjs';
 
 @Injectable({ providedIn: 'root' })
 export class UserService {
   constructor(private http: HttpClient) {}
 
+  findByEmail(email: string): Observable<User> {
+    return this.http.get<User>(
+      `${environment.API_URL}/${environment.API_VERSION}/user/find-by-email`,
+      {
+        params: new HttpParams().set('email', email),
+      }
+    );
+  }
+
   getAll() {
-    return this.http.get<User[]>(`${environment.API_URL}/${environment.API_VERSION}/users`);
+    return this.http.get<User[]>(`${environment.API_URL}/${environment.API_VERSION}/user/list`);
   }
 }
