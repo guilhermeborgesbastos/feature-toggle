ab5b2dbb80f10379de5f80f3d269753fe524c103
Guilherme Borges Bastos <guilhermeborgesbastos@gmail.com>
2020-06-25 18:43:32 -0300

    Created the common interfaces on the Angular app.
    - Created the IUser interface used as a contract for the user's model;
    - Created the IPageParams interface used as a contract for the Request's pageable model;
    - Created the IRestResponse interface used as a contract for the REST response's model;
    - Updated the User's model to implement the IUser interface.

--- src/main/resources/frontend/src/app/_models/user.ts
+++ src/main/resources/frontend/src/app/_models/user.ts
@@ -1,9 +1,11 @@
+import { IUser } from '@app/_shared/interfaces';
+
 export enum Role {
   SUPER_ADMIN = 'SUPER_ADMIN',
   PRODUCT_OWNER = 'PRODUCT_OWNER',
 }
 
-export class User {
+export class User implements IUser {
   id: number;
   role: Role;
   name: string;
---
+++ src/main/resources/frontend/src/app/_shared/interfaces.ts
@@ -0,0 +1,27 @@
+import { Role } from '@app/_models/user';
+
+export interface IUser {
+  id: number;
+  role: Role;
+  name: string;
+  email: string;
+  status: string;
+}
+
+export interface IPageParams {
+  page: number;
+  size: number;
+}
+
+export interface IRestResponse {
+  content: [];
+  empty: boolean;
+  first: boolean;
+  last: boolean;
+  number: number;
+  numberOfElements: number;
+  size: number;
+  totalElements: number;
+  totalPages: number;
+  sort: { sorted: boolean; unsorted: boolean; empty: boolean };
+}
