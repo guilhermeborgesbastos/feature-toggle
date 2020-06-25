7e3809d71daba206acf5b1f905b55af64dc97fe5
Guilherme Borges Bastos <guilhermeborgesbastos@gmail.com>
2020-06-25 18:56:16 -0300

    Created the Shared Module on the Angular app.
    - Created the shared module used to wrap up the common/shared resources.

---
+++ src/main/resources/frontend/src/app/_shared/shared.module.ts
@@ -0,0 +1,9 @@
+import { NgModule } from '@angular/core';
+
+import { CapitalizePipe } from './pipes/capitalize.pipe';
+
+@NgModule({
+  declarations: [CapitalizePipe],
+  exports: [CapitalizePipe],
+})
+export class SharedModule {}
