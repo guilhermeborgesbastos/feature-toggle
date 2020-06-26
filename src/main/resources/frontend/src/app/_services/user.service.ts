3f1c8a06343437fa0c08c65d4f67a11046f19461
Guilherme Borges Bastos <guilhermeborgesbastos@gmail.com>
2020-06-26 20:23:45 -0300

    Added the 'enable' and 'disable' methods to the User's service on the Angular app.
    - Created the 'enable' and 'disable' methods on the User's service;
    - Updated the User's service to use the IRestResponse interface.

--- src/main/resources/frontend/src/app/_services/user.service.ts
+++ src/main/resources/frontend/src/app/_services/user.service.ts
@@ -1,16 +1,16 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpParams, HttpHeaders } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 
 import { environment } from '@environments/environment';
 import { User, Role } from '@models/user';
 import { Observable } from 'rxjs';
-import { IPageParams } from '@app/_shared/interfaces';
+import { IPageParams, IUser, IRestResponse } from '@app/_shared/interfaces';
 
 @Injectable({ providedIn: 'root' })
 export class UserService {
   constructor(private http: HttpClient) {}
 
-  findByEmail(email: string): Observable<User> {
+  findByEmail(email: string): Observable<IUser> {
     return this.http.get<User>(
       `${environment.API_URL}/${environment.API_VERSION}/user/find-by-email`,
       {
@@ -19,12 +19,27 @@ export class UserService {
     );
   }
 
-  getAll(params: IPageParams) {
-    return this.http.get<User[]>(`${environment.API_URL}/${environment.API_VERSION}/user/list`, {
-      params: new HttpParams()
-        .set('page', params.page.toString())
-        .set('size', params.size.toString()),
-    });
+  getAll(params: IPageParams): Observable<IRestResponse<IUser>> {
+    return this.http.get<IRestResponse<IUser>>(
+      `${environment.API_URL}/${environment.API_VERSION}/user/list`,
+      {
+        params: new HttpParams()
+          .set('page', params.page.toString())
+          .set('size', params.size.toString()),
+      }
+    );
+  }
+
+  enable(userId: number): Promise<any> {
+    return this.http
+      .put(`${environment.API_URL}/${environment.API_VERSION}/user/${userId}/enable`, {})
+      .toPromise();
+  }
+
+  disable(userId: number): Promise<any> {
+    return this.http
+      .put(`${environment.API_URL}/${environment.API_VERSION}/user/${userId}/disable`, {})
+      .toPromise();
   }
 
   signIn(
