4178a1f7f95ca172d56a0181bf312769608585a7
Guilherme Borges Bastos <guilhermeborgesbastos@gmail.com>
2020-06-28 16:30:46 -0300

    Created the 'ApiError' interface to the Angular app.
    - Created the ApiError contract to consume the messages caught by the REST Exception Handler.

--- src/main/resources/frontend/src/app/_shared/interfaces.ts
+++ src/main/resources/frontend/src/app/_shared/interfaces.ts
@@ -25,3 +25,31 @@ export interface IRestResponse<T> {
   totalPages: number;
   sort: { sorted: boolean; unsorted: boolean; empty: boolean };
 }
+export interface ApiError {
+  debugMessage?: string;
+  message: string;
+  status: string;
+  subErrors: [
+    {
+      field: string;
+      message: string;
+      object: string;
+      rejectedValue: string;
+    }
+  ];
+  timestamp: string;
+}
+
+export function formatError(httpError: any): string {
+  if (httpError && httpError.error && httpError.error.apierror) {
+    const error = httpError.error.apierror;
+    console.log(error);
+    let msg = error.message;
+    for (let i = 0; i < error.subErrors.length; i++) {
+      const e = error.subErrors[i];
+      msg += `: ${e.message} on ${e.field}\n`;
+    }
+    return msg;
+  }
+  return httpError.message ? httpError.message : 'connection problem with server';
+}
