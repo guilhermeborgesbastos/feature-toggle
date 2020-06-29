a84372db8aff16a341d3bf0a2ea67144a1d0e4b2
Guilherme Borges Bastos <guilhermeborgesbastos@gmail.com>
2020-06-29 18:16:42 -0300

    Created the utils file on the Angular application.
    -  Created a utiliarian file with common functions.

---
+++ src/main/resources/frontend/src/app/_helpers/utils.ts
@@ -0,0 +1,15 @@
+import { HttpErrorResponse } from '@angular/common/http';
+import { throwError } from 'rxjs';
+
+export function handleError(error: HttpErrorResponse) {
+  if (error.error instanceof ErrorEvent) {
+    // A client-side or network error occurred. Handle it accordingly.
+    console.error('An error occurred:', error.error.message);
+  } else {
+    // The backend returned an unsuccessful response code.
+    // The response body may contain clues as to what went wrong,
+    console.error(`Backend returned code ${error.status}, ` + `body was: ${error.error}`);
+  }
+  // return an observable with a customer-facing error message
+  return throwError('Something bad happened; please try again later.');
+}
