c2448a82162a7f8faa402ed497f032a662eb0ab3
Guilherme Borges Bastos <guilhermeborgesbastos@gmail.com>
2020-06-30 19:24:45 -0300

    Created the customer's child routing on the Angular app.

---
+++ src/main/resources/frontend/src/app/customer/customer-routing.module.ts
@@ -0,0 +1,30 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { CustomerAddComponent } from './customer-add/customer-add.component';
+import { CustomersListComponent } from './customers-list/customers-list.component';
+import { Role } from '@app/_models/user';
+import { CustomerEditComponent } from './customer-edit/customer-edit.component';
+
+const routes: Routes = [
+  {
+    path: 'customers',
+    component: CustomersListComponent,
+    data: { roles: [Role.SUPER_ADMIN, Role.PRODUCT_OWNER], title: 'Customers list' },
+  },
+  {
+    path: 'customer/new',
+    component: CustomerAddComponent,
+    data: { roles: [Role.SUPER_ADMIN, Role.PRODUCT_OWNER], title: 'Create customer' },
+  },
+  {
+    path: 'customer/edit/:customer_id',
+    component: CustomerEditComponent,
+    data: { roles: [Role.SUPER_ADMIN, Role.PRODUCT_OWNER], title: 'Edit customer' },
+  },
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule],
+})
+export class CustomerRoutingModule {}
