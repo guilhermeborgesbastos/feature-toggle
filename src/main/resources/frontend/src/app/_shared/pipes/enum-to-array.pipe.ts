0390a741ae7a4c70cff51bda97083db89a0cd236
Guilherme Borges Bastos <guilhermeborgesbastos@gmail.com>
2020-07-01 18:13:24 -0300

    Updated the 'enumToArray' pipe to a declarative implementation on the Angular App.

--- src/main/resources/frontend/src/app/_shared/pipes/enum-to-array.pipe.ts
+++ src/main/resources/frontend/src/app/_shared/pipes/enum-to-array.pipe.ts
@@ -3,12 +3,11 @@ import { Pipe, PipeTransform } from '@angular/core';
 @Pipe({ name: 'enumToArray' })
 export class EnumToArrayPipe implements PipeTransform {
   transform(value, args?: string[]): any {
-    let result = [];
-    const keys = Object.keys(value);
-    const values = Object.values(value);
-    for (let i = 0; i < keys.length; i++) {
-      result.push({ key: keys[i], value: values[i] });
-    }
+    const result = Object.keys(value)
+      .filter((e) => !isNaN(+e))
+      .map((o) => {
+        return { key: +o, value: value[o] };
+      });
     return result.sort((a, b) => (a.value < b.value ? -1 : 1));
   }
 }
