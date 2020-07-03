5e5bc9ba3cfd21750466c6cb0bb328cb00320d6b
Guilherme Borges Bastos <guilhermeborgesbastos@gmail.com>
2020-07-03 16:52:36 -0300

    Created the feature routing on the Angular App.

---
+++ src/main/resources/frontend/src/app/feature/feature-routing.module.ts
@@ -0,0 +1,30 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { FeatureAddComponent } from './feature-add/feature-add.component';
+import { FeaturesListComponent } from './features-list/features-list.component';
+import { Role } from '@app/_models/user';
+import { FeatureEditComponent } from '@app/feature/feature-edit/feature-edit.component';
+
+const routes: Routes = [
+  {
+    path: 'features',
+    component: FeaturesListComponent,
+    data: { roles: [Role.SUPER_ADMIN], title: 'Features list' },
+  },
+  {
+    path: 'feature/new',
+    component: FeatureAddComponent,
+    data: { roles: [Role.SUPER_ADMIN], title: 'Create feature' },
+  },
+  {
+    path: 'feature/edit/:feature_id',
+    component: FeatureEditComponent,
+    data: { roles: [Role.SUPER_ADMIN], title: 'Edit feature' },
+  },
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule],
+})
+export class FeatureRoutingModule {}
