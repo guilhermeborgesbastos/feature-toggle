8fc0ad592a0a878297b31e1e064fc24c78df8cde
Guilherme Borges Bastos <guilhermeborgesbastos@gmail.com>
2020-07-03 16:47:54 -0300

    Crated the feature service on the Angular App.
    - Created the Feature Service to provide ways of connection the app with the feature resource on the REST API;

---
+++ src/main/resources/frontend/src/app/_services/feature.service.ts
@@ -0,0 +1,58 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+import { environment } from '@environments/environment';
+import { Observable } from 'rxjs';
+import { IFeature, AbstractApiService } from '@app/_shared/interfaces';
+
+@Injectable({ providedIn: 'root' })
+export class FeatureService extends AbstractApiService<IFeature> {
+  private resourceUrl: string;
+
+  constructor(http: HttpClient) {
+    super(http, `${environment.API_URL}/${environment.API_VERSION}/feature/list`);
+    this.resourceUrl = `${environment.API_URL}/${environment.API_VERSION}/feature`;
+  }
+
+  enable(featureId: number): Promise<any> {
+    return this.http.put(`${this.resourceUrl}/${featureId}/enable`, {}).toPromise();
+  }
+
+  archive(featureId: number): Promise<any> {
+    return this.http.put(`${this.resourceUrl}/${featureId}/archive`, {}).toPromise();
+  }
+
+  create(
+    displayName: string,
+    technicalName: string,
+    description: string,
+    inverted: boolean = false,
+    expiresOn?: string,
+    customerIds?: number[]
+  ): Promise<any> {
+    const data = {
+      displayName,
+      technicalName,
+      description,
+      inverted,
+      expiresOn,
+      customerIds,
+    };
+    debugger;
+    return this.http
+      .post(`${environment.API_URL}/${environment.API_VERSION}/feature`, data)
+      .toPromise();
+  }
+
+  update(feature: IFeature): Observable<any> {
+    return this.http.put(`${this.resourceUrl}/${feature.id}`, feature);
+  }
+
+  findById(featureId: number): Observable<IFeature> {
+    return this.http.get<IFeature>(`${this.resourceUrl}/${featureId}`);
+  }
+
+  delete(featureId: number): Promise<any> {
+    return this.http.delete<any>(`${this.resourceUrl}/${featureId}`).toPromise();
+  }
+}
