8f9a2e0f52fa64cb32604b09307d7a50427dc846
Guilherme Borges Bastos <guilhermeborgesbastos@gmail.com>
2020-07-05 13:34:11 -0300

    Removed the 'Archive' feature from the features table on the Angular App.
    - Removed the 'archive' feature from the features pageable table because the archive action requests the user id.

--- src/main/resources/frontend/src/app/feature/features-list/features-list.component.ts
+++ src/main/resources/frontend/src/app/feature/features-list/features-list.component.ts
@@ -7,6 +7,7 @@ import { SnackBarService } from '@app/_services/snack-bar.service';
 import { IFeature } from '@app/_shared/interfaces';
 import { AbstractDataSource } from '@app/_shared/abstract-data.source';
 import { Router } from '@angular/router';
+import { Feature } from '@app/_models/feature';
 
 @Component({
   selector: 'app-features-list',
@@ -42,41 +43,25 @@ export class FeaturesListComponent implements OnInit, AfterViewInit {
     this.dataSource.load();
   }
 
-  private enableFeature(featureId: number) {
-    return this.featureService.enable(featureId).then(
-      () => this.snackBarService.show(true, 'Feature has been enabled successfully.'),
-      (error) => catchError(handleError)
-    );
-  }
-
-  private disableFeature(featureId: number) {
-    return this.featureService.disable(featureId).then(
-      () => this.snackBarService.show(true, 'Feature has been disabled successfully.'),
-      (error) => catchError(handleError)
-    );
-  }
-
-  public onToggleChange(checked: boolean, featureId: number): void {
-    if (checked) {
-      this.enableFeature(featureId);
-    } else {
-      this.disableFeature(featureId);
-    }
-  }
-
   public edit(featureId: number) {
     this.router.navigate(['/feature/edit', featureId]);
   }
 
-  public archive(featureId: number) {
-    this.router.navigate(['/feature/edit', featureId]);
+  public onToggleChange(checked: boolean, featureId: number): void {
+    const data: IFeature = new Feature();
+    data.id = featureId;
+    data.inverted = checked;
+    this.featureService.update(data).subscribe(
+      () => this.snackBarService.show(true, 'Feature invertion has been switched successfully.'),
+      (error) => catchError(handleError)
+    );
   }
 
   public delete(featureId: number) {
     this.featureService.delete(featureId).then(
       (res) => {
         this.snackBarService.show(true, `Feature has been deleted.`);
-        // Realoding table content
+        // Realoding table of content
         this.dataSource.load();
       },
       (err) =>
