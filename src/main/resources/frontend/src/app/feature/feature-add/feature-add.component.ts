33406372124cf8ae58d72c3eb49f46b4a9f066fe
Guilherme Borges Bastos <guilhermeborgesbastos@gmail.com>
2020-07-07 17:21:11 -0300

    Updated the feature add component to use the 'ChipListComponent' on the Angular App.

--- src/main/resources/frontend/src/app/feature/feature-add/feature-add.component.ts
+++ src/main/resources/frontend/src/app/feature/feature-add/feature-add.component.ts
@@ -5,10 +5,7 @@ import { FeatureService } from '@services/feature.service';
 import { formatError } from '@helpers/utils';
 import { BehaviorSubject, Observable } from 'rxjs';
 import { SnackBarService } from '@app/_services/snack-bar.service';
-import { MatAutocompleteSelectedEvent, MatAutocomplete } from '@angular/material/autocomplete';
-import { ICustomer, IRestResponse } from '@app/_shared/interfaces';
-import { startWith, map } from 'rxjs/operators';
-import { CustomerService } from '@app/_services/customer.service';
+import { CustomerChipListComponent } from '@app/customer/customer-chip-list/customer-chip-list.component';
 
 @Component({
   selector: 'app-feature-add',
@@ -17,43 +14,23 @@ import { CustomerService } from '@app/_services/customer.service';
 })
 export class FeatureAddComponent implements OnInit {
   private loadingSubject: BehaviorSubject<boolean>;
+
   loading$: Observable<boolean>;
-
   createForm: FormGroup;
-  customerCtrl = new FormControl();
 
-  selectable = true;
-  removable = true;
-
-  filteredCustomers: Observable<ICustomer[]>;
-
-  allCustomers: ICustomer[] = [];
-  selectedCustomers: ICustomer[] = [];
-
-  @ViewChild('customersInput', { static: false }) customersInput: ElementRef<HTMLInputElement>;
   @ViewChild('description', { static: false }) description: ElementRef<HTMLInputElement>;
-  @ViewChild('auto', { static: false }) matAutocomplete: MatAutocomplete;
+  @ViewChild('chipList') chipList: CustomerChipListComponent;
 
   constructor(
     private router: Router,
     private featureService: FeatureService,
-    private customerService: CustomerService,
     private snackBarService: SnackBarService
   ) {
     this.loadingSubject = new BehaviorSubject<boolean>(false);
     this.loading$ = this.loadingSubject.asObservable();
-
-    this.filteredCustomers = this.customerCtrl.valueChanges.pipe(
-      startWith(null),
-      map((search: ICustomer | null) => (search ? this.filter(search) : this.allCustomers.slice()))
-    );
   }
 
   ngOnInit() {
-    this.customerService
-      .load({ page: 0, size: 100 }) // TODO: Apply a server side filtering...
-      .subscribe((resp: IRestResponse<ICustomer>) => (this.allCustomers = resp.content));
-
     this.createForm = new FormGroup({
       displayName: new FormControl(''),
       technicalName: new FormControl('', Validators.required),
@@ -62,45 +39,18 @@ export class FeatureAddComponent implements OnInit {
     });
   }
 
-  private filter(value: any): ICustomer[] {
-    const filterValue = value && value.name ? value.name : value;
-    return this.allCustomers.filter(
-      (cust) => cust.name.toLowerCase().indexOf(filterValue.toLowerCase()) !== -1
-    );
-  }
-
-  remove(customer: ICustomer): void {
-    const index = this.selectedCustomers.indexOf(customer);
-
-    if (index > -1) {
-      this.selectedCustomers.splice(index, 1);
-    }
-  }
-
-  selected(event: MatAutocompleteSelectedEvent): void {
-    const customer: ICustomer = event.option.value;
-    const index = this.selectedCustomers.indexOf(customer);
-
-    if (index === -1) {
-      this.selectedCustomers.push(customer);
-      this.customersInput.nativeElement.value = '';
-      this.customerCtrl.setValue(null);
-    }
-  }
-
   create() {
-    const customerIds: number[] = this.selectedCustomers.map((cust) => cust.id);
     this.featureService
       .create(
         this.createForm.get('displayName').value,
         this.createForm.get('technicalName').value,
+        this.description.nativeElement.value,
         this.createForm.get('inverted').value,
         this.createForm.get('expiresOn').value,
-        this.description.nativeElement.value,
-        customerIds
+        this.chipList.retrieveCustomerIds()
       )
       .then(
-        (newFeature) => {
+        (feature) => {
           this.loadingSubject.next(false);
           this.snackBarService.show(true, `Feature has been successfully created.`);
           this.router.navigate(['/features']);
