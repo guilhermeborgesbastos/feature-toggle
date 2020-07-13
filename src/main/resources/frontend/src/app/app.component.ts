51438d6f4a872ef910273549e3ff09e635868f55
Guilherme Borges Bastos <guilhermeborgesbastos@gmail.com>
2020-07-13 17:13:46 -0300

    Updated the app's sidebar to use its own service on the Angular App.
    - Updated the application's sidebar to be opened by default;
    - Updated the 'hamburger menu' to turns into a 'close' icon depending on the sidebar state (opened or closed).

--- src/main/resources/frontend/src/app/app.component.ts
+++ src/main/resources/frontend/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
+import { Component, OnInit, ViewChild, OnDestroy } from '@angular/core';
 
 import { AuthenticationService } from '@services/authentication.service';
 import { User } from '@models/user';
@@ -8,34 +8,49 @@ import { SnackBarService } from './_services/snack-bar.service';
 import { Title } from '@angular/platform-browser';
 import { Router, NavigationEnd, ActivatedRoute } from '@angular/router';
 import { environment } from '../environments/environment';
+import { MatDrawer } from '@angular/material/sidenav';
+import { SidebarService } from './_services/sidebar.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent implements OnInit {
-  @ViewChild('drawer') drawer;
+export class AppComponent implements OnInit, OnDestroy {
+  sidebarService: SidebarService;
+  matDrawer: MatDrawer;
+
+  @ViewChild(MatDrawer) set matDrawerView(matDrawer: MatDrawer) {
+    if (matDrawer) {
+      this.matDrawer = matDrawer;
+      this.sidebarService.setMatDrawer(matDrawer);
+    }
+  }
 
   title: string;
   appVersion: string;
   loggedUser$: Observable<User>;
   authService: AuthenticationService;
+  isDrawerOpened: boolean;
 
   constructor(
     private titleService: Title,
     private router: Router,
     private activatedRoute: ActivatedRoute,
     private snackBarService: SnackBarService,
+    sidebarService: SidebarService,
     authService: AuthenticationService
   ) {
     this.authService = authService;
     this.appVersion = environment.APP_VERSION;
+    this.sidebarService = sidebarService;
+    this.isDrawerOpened = true;
   }
 
   ngOnInit() {
     this.loggedUser$ = this.authService.loggedUser$;
-    this.authService.logout$.subscribe((msg) => this.snackBarService.show(true, msg));
+    this.authService.logout$.subscribe((msg: string) => this.snackBarService.show(true, msg));
+    this.sidebarService.isOpened.subscribe((isOpened: boolean) => (this.isDrawerOpened = isOpened));
 
     /*
      * It subscribe to router events and access the data with the help of ActivatedRoute and
@@ -61,6 +76,10 @@ export class AppComponent implements OnInit {
         this.titleService.setTitle(tabTitle);
         this.title = title;
       });
+  }
+
+  ngOnDestroy() {
+    this.sidebarService.isOpened.unsubscribe();
   }
 
   logout() {
