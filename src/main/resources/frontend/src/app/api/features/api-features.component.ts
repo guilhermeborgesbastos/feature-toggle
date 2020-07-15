1f82ebedacef6ffe3955ec72d60594434f42ee5f
Guilherme Borges Bastos <guilhermeborgesbastos@gmail.com>
2020-07-15 17:01:40 -0300

    Created the blank 'APIs Feature' component on the Angular App.

---
+++ src/main/resources/frontend/src/app/api/features/api-features.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-api-features',
+  templateUrl: './api-features.component.html',
+  styleUrls: ['./api-features.component.scss'],
+})
+export class ApiFeaturesComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+}
