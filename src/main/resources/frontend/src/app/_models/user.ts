5091b9ad88ff0ebfef731de5d7272a8c831fb6a9
Guilherme Borges Bastos <guilhermeborgesbastos@gmail.com>
2020-07-24 16:28:44 -0300

    Included the 'CLIENT' as a role on the Angular App.

--- src/main/resources/frontend/src/app/_models/user.ts
+++ src/main/resources/frontend/src/app/_models/user.ts
@@ -2,7 +2,8 @@ import { IUser } from '@app/_shared/interfaces';
 
 export enum Role {
   SUPER_ADMIN = 1, // The SUPER ADMIN has the ROLE ID equals to 1 on the API
-  PRODUCT_OWNER = 2, // The SUPER PRODUCT_OWNER has the ROLE ID equals to 2 on the API
+  PRODUCT_OWNER = 2, // The PRODUCT_OWNER has the ROLE ID equals to 2 on the API
+  CLIENT = 3, // The CLIENT has the ROLE ID equals to 3 on the API
 }
 
 export class User implements IUser {
