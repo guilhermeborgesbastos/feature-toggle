04a14f9285a0dfe3e3da5dab29d9ab6d89631082
Guilherme Borges Bastos <guilhermeborgesbastos@gmail.com>
2020-07-27 17:14:32 -0300

    Updated the 'home component' with a short project description on the Angular App.

--- src/main/resources/frontend/src/app/home/home.component.ts
+++ src/main/resources/frontend/src/app/home/home.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 
-@Component({ templateUrl: 'home.component.html' })
+@Component({ templateUrl: 'home.component.html', styleUrls: ['./home.component.scss'] })
 export class HomeComponent {
   ngOnInit() {}
 }
