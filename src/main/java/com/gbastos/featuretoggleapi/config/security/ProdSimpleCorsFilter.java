a4cb4b2f430d05478cd45b7744957cfa15348cb1
Guilherme Borges Bastos <guilhermeborgesbastos@gmail.com>
2020-07-29 10:34:25 -0300

    Created the production CORS filter in the REST API.

---
+++ src/main/java/com/gbastos/featuretoggleapi/config/security/ProdSimpleCorsFilter.java
@@ -0,0 +1,40 @@
+package com.gbastos.featuretoggleapi.config.security;
+
+import org.springframework.context.annotation.Profile;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Component
+@Order(Ordered.HIGHEST_PRECEDENCE)
+@Profile("prod")
+public class ProdSimpleCorsFilter implements Filter {
+
+  @Override
+  public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain)
+      throws IOException, ServletException {
+    HttpServletResponse response = (HttpServletResponse) res;
+    HttpServletRequest request = (HttpServletRequest) req;
+    response.setHeader("Access-Control-Allow-Origin", "http://featuretoggle.guilhermeborgesbastos.com");
+    response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE, PUT");
+    response.setHeader("Access-Control-Allow-Headers", "x-requested-with, authorization, Content-Type, Authorization, credential, X-XSRF-TOKEN");
+    response.setHeader("Access-Control-Max-Age", "3600");
+
+    if ("OPTIONS".equalsIgnoreCase(request.getMethod())) {
+      response.setStatus(HttpServletResponse.SC_OK);
+    } else {
+      chain.doFilter(req, res);
+    }
+  }
+
+  @Override
+  public void init(FilterConfig filterConfig) {}
+
+  @Override
+  public void destroy() {}
+}
