d762baf9dd3d460ad4b7ec9f35ff65435685a65c
Guilherme Borges Bastos <guilhermeborgesbastos@gmail.com>
2020-07-30 14:45:18 -0300

    Updated the CORs filter on production mode to use SSL on the REST API.

--- src/main/java/com/gbastos/featuretoggleapi/config/security/ProdSimpleCorsFilter.java
+++ src/main/java/com/gbastos/featuretoggleapi/config/security/ProdSimpleCorsFilter.java
@@ -20,7 +20,7 @@ public class ProdSimpleCorsFilter implements Filter {
       throws IOException, ServletException {
     HttpServletResponse response = (HttpServletResponse) res;
     HttpServletRequest request = (HttpServletRequest) req;
-    response.setHeader("Access-Control-Allow-Origin", "http://featuretoggle.guilhermeborgesbastos.com");
+    response.setHeader("Access-Control-Allow-Origin", "https://featuretoggle.guilhermeborgesbastos.com");
     response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE, PUT");
     response.setHeader("Access-Control-Allow-Headers", "x-requested-with, authorization, Content-Type, Authorization, credential, X-XSRF-TOKEN");
     response.setHeader("Access-Control-Max-Age", "3600");
