cb96afaed57e28b858d3bccf8f8f4a9997a7e96e
Guilherme Borges Bastos <guilhermeborgesbastos@gmail.com>
2020-08-13 16:44:18 -0300

    Created 'barrel' for the Angular pipes.

---
+++ src/main/resources/frontend/src/app/_shared/pipes/index.ts
@@ -0,0 +1,7 @@
+export * from './capitalize.pipe';
+export * from './default.pipe';
+export * from './ellipsis.pipe';
+export * from './empty.pipe';
+export * from './enum-to-array.pipe';
+export * from './loading.pipe';
+export * from './normalize-enum.pipe';
